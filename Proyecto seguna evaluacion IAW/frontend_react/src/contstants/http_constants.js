4d5a69fb5b0d70afa913cba73403eed4ad9e862f
lmasmunyoz <129276330+lmasmunyoz@users.noreply.github.com>
2023-06-02 09:21:34 +0200

    Delete Proyecto seguna evaluacion IAW directory

--- Proyecto seguna evaluacion IAW/frontend_react/src/contstants/http_constants.js
+++
@@ -1,7 +0,0 @@
-export const URL_API='http://127.0.0.1:8000/api/';
-export const URL_API_FURNITURE='http://127.0.0.1:8000/api/furnitures';
-export const URL_API_GATEGORIES='http://127.0.0.1:8000/api/categories';
-export const HEADERS={
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-}
